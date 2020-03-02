b8f5bdaea0113e9f5f1f9abaf9d19d48d74b1f4f
Erin Stansbury <ekstansbury@gmail.com>
2020-03-02 13:48:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       elsif @sender.balance < @amount
         @status = "rejected"
         puts "Transaction rejected. Please check your account balance."
-      else @sender.balance > @amount && @status == "pending"
+      else @sender.balance > @amount
         @sender.balance -= @amount
         @receiver.balance += @amount
         @status = "complete"
