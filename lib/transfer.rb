933d7bc43f762f87f621d914708ec8e7d3f3fd73
Erin Stansbury <ekstansbury@gmail.com>
2020-03-02 13:51:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,8 @@ class Transfer
 
   def execute_transaction
     if self.valid? && self.status == "pending" && @sender.balance > @amount
-      receiver.balance += @amount
-      sender.balance -= @amount
+      @receiver.balance += @amount
+      @sender.balance -= @amount
       self.status = "complete"
     else
       self.status = "rejected"
